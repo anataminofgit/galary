dcc86b9509e4f1371e566e9a3ec8c52379ece8cc
anataminofgit <anat.aminof.test@gmail.com>
2020-08-21 17:06:53 +0300

    remove debug consol.log

--- src/reducer/AlbumReducer.js
+++ src/reducer/AlbumReducer.js
@@ -1,15 +1,12 @@
 
 const SET_ALBUMS = "SET_ALBUMS";
 
-const initialAlbums = {albumsList: []};
+const initialAlbums = { albumsList: [] };
 
 const albumReducer = (state = initialAlbums, action) => {
-    console.log("action", action);
-
     switch (action.type) {
         case SET_ALBUMS:
             {
-                console.log("action", action)
                 return {
                     ...state,
                     albumsList: [...action.albumsList]
